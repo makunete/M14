05ce8afdc72e011cbf16cb65b94815e19192e80e
makunete <mmarcsola@gmail.com>
2016-12-19 20:16:24 +0100

    Add files via upload

--- mysite2/forum/admin.py
+++ mysite2/forum/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Login
+
+admin.site.register(Login)
+
+from .models import Missatges
+
+admin.site.register(Missatges)
+
+from .models import Hashtags
+
+admin.site.register(Hashtags)
